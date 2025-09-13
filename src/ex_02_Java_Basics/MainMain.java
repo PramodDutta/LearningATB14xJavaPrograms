b42cb4a16f214520c78c0c2202193f1700a59a6f
Promode <pramoddutta_96@yahoo.com>
2025-09-13 11:08:05 +0530

    Part 13th Java

--- src/ex_02_Java_Basics/MainMain.java
+++ src/ex_02_Java_Basics/MainMain.java
@@ -3,5 +3,9 @@ package ex_02_Java_Basics;
 public class MainMain {
     public static void main(String[] args) {
         System.out.println(10+3/2-1*5);
+        System.out.println(10+3/2-1*5);
+        System.out.println(10+3/2-1*5);
+        System.out.println(10+3/2-1*5);
+        System.out.println(10+3/2-1*5);
     }
 }
